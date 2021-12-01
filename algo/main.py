4ca0c83bf3c7624e50a671ad0e7b500542dcba1f
alex-straw <as17163@bristol.ac.uk>
2021-12-01 13:16:02 +0000

    using list comphrehensions to extract relevant values from list of lists - to insert into bid ask data frame

--- algo/main.py
+++ algo/main.py
@@ -5,7 +5,21 @@ import config
 parameters = {'product': 'BTC-USD',
               'level': 2,
               'run_duration': 5,
-              'lob_update_time': 0.5}
+              'lob_update_interval': 0.5,
+              'bid_ask_depth': 5}
+
+
+def get_relevant_bid_asks(lob, depth):
+    """ create a data frame of just the best bids and asks up to a specific depth (parameters)"""
+    data = {
+            'bid': [item[0] for item in lob['bids'][0:depth]],
+            'bid_qty' : [item[1] for item in lob['bids'][0:depth]],
+            'ask': [item[0] for item in lob['asks'][0:depth]],
+            'ask_qty' : [item[1] for item in lob['asks'][0:depth]]
+            }
+
+    bid_ask_df = pd.DataFrame(data=data)
+    return bid_ask_df
 
 
 def get_lob(client, product, level):
@@ -28,8 +42,12 @@ def main(parameters):
         # Retrieve lob
         lob = get_lob(auth_client, parameters['product'], parameters['level'])
 
+        bid_ask_df = get_relevant_bid_asks(lob, depth=parameters['bid_ask_depth'])
+
+        print(bid_ask_df)
+
         # Wait a set duration until requesting lob again
-        time.sleep(parameters['lob_update_time'])
+        time.sleep(parameters['lob_update_interval'])
 
         # Terminate algorithm after chosen duration
         if elapsed_time > parameters['run_duration']:

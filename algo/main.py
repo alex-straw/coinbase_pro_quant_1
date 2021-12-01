9fc69b389355ae2006976ae442439e4bffcd5094
alex-straw <as17163@bristol.ac.uk>
2021-12-01 12:37:46 +0000

    retrieving lob at given intervals described in parameters.  Terminates after a set duration

--- algo/main.py
+++ algo/main.py
@@ -2,9 +2,10 @@ import cbpro, time
 import pandas as pd
 import config
 
-
 parameters = {'product': 'BTC-USD',
-              'level': 2}
+              'level': 2,
+              'run_duration': 5,
+              'lob_update_time': 0.5}
 
 
 def get_lob(client, product, level):
@@ -16,10 +17,25 @@ def get_lob(client, product, level):
 def main(parameters):
     """ get the authenticated API client, and retrieve LOB for specific market """
     auth_client = cbpro.AuthenticatedClient(config.key, config.b64secret, config.passphrase)
-    lob = get_lob(auth_client, parameters['product'], parameters['level'])
-    print(lob['bids'])
+
+    start_time = time.time()
+
+    while True:
+        # Only run for a set duration
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+
+        # Retrieve lob
+        lob = get_lob(auth_client, parameters['product'], parameters['level'])
+
+        # Wait a set duration until requesting lob again
+        time.sleep(parameters['lob_update_time'])
+
+        # Terminate algorithm after chosen duration
+        if elapsed_time > parameters['run_duration']:
+            print("Algorithm run time: " + str(int(elapsed_time)) + " seconds")
+            break
 
 
 if __name__ == "__main__":
     main(parameters)
-
